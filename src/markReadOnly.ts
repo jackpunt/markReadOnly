871b8bdee2bf982d402efcbc204afb382f272477
jackpunt <jpeckj@gmail.com>
2022-09-29 18:08:43 -0700

    0.0.8: readonlyTrue, readonlyFalse, readonlyNull, readonlyToggle

--- src/markReadOnly.ts
+++ src/markReadOnly.ts
@@ -11,23 +11,25 @@ type ReadonlyPath = { [path: string]: boolean | null } | undefined;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	let setCmd = vscode.commands.registerCommand('markreadonly.setReadonly', (...args) => {
-    let arg = args[0] as string;
-    let value: boolean | 'toggle' = arg === 'toggle' ? arg : arg === 'false' ? false : true;
-    vscode.window.activeTextEditor && setReadOnly(vscode.window.activeTextEditor.document, value);
-	});
-	let clrCmd = vscode.commands.registerCommand('markreadonly.clearReadonly', () => {
-    vscode.window.activeTextEditor && setReadOnly(vscode.window.activeTextEditor.document, null);
-	});
-	let toggleCmd = vscode.commands.registerCommand('markreadonly.toggleReadonly', () => {
-    vscode.window.activeTextEditor && setReadOnly(vscode.window.activeTextEditor.document, 'toggle');
-	});
-	context.subscriptions.push(setCmd, clrCmd, toggleCmd);
+  let setCmdTrue = vscode.commands.registerTextEditorCommand('markreadonly.readonlyTrue', (editor) => {
+    setReadOnly(editor.document, true);
+  });
+  let setCmdFalse = vscode.commands.registerTextEditorCommand('markreadonly.readonlyFalse', (editor) => {
+    setReadOnly(editor.document, false);
+  });
+  let clrCmd = vscode.commands.registerTextEditorCommand('markreadonly.readonlyNull', (editor) => {
+    setReadOnly(editor.document, null);
+  });
+  let toggleCmd = vscode.commands.registerTextEditorCommand('markreadonly.readonlyToggle', (editor) => {
+    setReadOnly(editor.document, 'toggle');
+  });
+  context.subscriptions.push(setCmdTrue, setCmdFalse, clrCmd, toggleCmd);
 
   function setReadOnly(doc: vscode.TextDocument, value: boolean | null | 'toggle') {
     console.log(`setReadOnly: doc.fsPath=${doc.fileName}`);
-    const inGlobs = ["/settings/folder", "**/settings.json"];
-    if (inGlobs.find(glob => matchGlob(doc, glob))) { return; } // IGNORE WHEN EDITING settings.json
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const noGlobs = { '/settings/folder': true, '**/settings.json': true };
+    if (anyGlobMatches(noGlobs, doc)) { return; } // IGNORE WHEN EDITING settings.json
     const path = doc.fileName;
     const key = 'files.readonlyPath';
     const [section, rest] = key.split('.', 2);
@@ -35,13 +37,17 @@ export function activate(context: vscode.ExtensionContext) {
     const pathValue = config.get<ReadonlyPath>(rest);
     const oldValue = pathValue && pathValue[path];
     const newValue = (value === 'toggle') ? !oldValue : value;
-    const keyValue: ReadonlyPath = {} ; keyValue[path] = newValue;
+    const keyValue: ReadonlyPath = {}; keyValue[path] = newValue;
     vscode.workspace.getConfiguration().update(key, keyValue, false); // false: WORKSPACE, true: GLOBAL
     vscode.window.showInformationMessage(`setReadOnly: ${value} [${oldValue} -> ${newValue}]`);
   }
 
-   function matchGlob(document: vscode.TextDocument, glob: string) {
-    return vscode.languages.match({ pattern: glob }, document) !== 0;
+  function editorForDocument(doc: vscode.TextDocument) {
+    return vscode.window.visibleTextEditors.find((editor) => editor.document === doc);
+  }
+
+  function anyGlobMatches(globs: ReadonlyPath, document: vscode.TextDocument) {
+    return !!(globs && Object.keys(globs).find(glob => (globs[glob] === true) && (vscode.languages.match({ pattern: glob }, document) !== 0)));
   }
 }
 
